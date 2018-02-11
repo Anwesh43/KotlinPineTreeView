a8ed3ca2d99a0b851b4373ca7053df8db3f73d4a
Anwesh43 <anweshthecool0@gmail.com>
2018-02-11 11:46:04 +0530

    adding PineTree class

--- pinetreeview/src/main/java/ui/anwesome/com/pinetreeview/PineTreeView.kt
+++ pinetreeview/src/main/java/ui/anwesome/com/pinetreeview/PineTreeView.kt
@@ -19,4 +19,24 @@ class PineTreeView(ctx:Context):View(ctx) {
         }
         return true
     }
+   data  class PineTree(var x:Float,var y:Float,var size:Float) {
+       fun draw(canvas:Canvas,paint:Paint) {
+           canvas.save()
+           canvas.translate(x,y)
+           for(i in 0..1) {
+               canvas.save()
+               canvas.rotate(60f*(i*2 - 1))
+               canvas.drawLine(0f,0f,0f,size,paint)
+               canvas.restore()
+           }
+           canvas.restore()
+       }
+       fun update(stopcb:(Float)->Unit) {
+
+       }
+       fun startUpdating(startcb:()->Unit) {
+
+       }
+   }
+
 }
