b3ad74a3ab192336c310962aa3243d97ebf29dd9
Anwesh43 <anweshthecool0@gmail.com>
2018-02-11 15:17:18 +0530

    using SelectionListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlinpinetreeview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinpinetreeview/MainActivity.kt
@@ -2,12 +2,16 @@ package ui.anwesome.com.kotlinpinetreeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.pinetreeview.PineTreeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        PineTreeView.create(this)
+        val view = PineTreeView.create(this)
+        view.addPineTreeListener {
+            Toast.makeText(this,"selected $it",Toast.LENGTH_SHORT).show()
+        }
     }
 }
